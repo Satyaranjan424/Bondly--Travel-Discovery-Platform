d98e03e25389481442f2c665afe17cd535bb3a82
Satya Ranjan Das <satya.das@appman.tech>
2026-05-09 23:32:20 +0530

    Deploy API on Vercel serverless

--- api/[...route].js
+++ api/[...route].js
@@ -10,10 +10,20 @@ const getApp = () => {
   return appPromise;
 };
 
-export default async function handler(request) {
+async function handleRequest(request) {
   const app = await getApp();
   const url = new URL(request.url);
   url.pathname = url.pathname.replace(/^\/api/, "") || "/";
 
   return app.fetch(new Request(url, request));
 }
+
+export const GET = handleRequest;
+export const POST = handleRequest;
+export const PUT = handleRequest;
+export const DELETE = handleRequest;
+export const OPTIONS = handleRequest;
+
+export default {
+  fetch: handleRequest,
+};
